c99df7fff88ad8322930a95cd17000316a79826f
SillyCat96 <alexey.kozhevatov@mail.ru>
2024-05-22 10:41:53 +0300

    тест 1

--- testing1.py
+++ testing1.py
@@ -1,1 +1,13 @@
-print("Hello, world!") 
+def fibonacci(n):
+    if n <= 0:
+        return "Некорректный ввод"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# Пример использования функции для вычисления 10 чисел Фибоначчи
+for i in range(1, 11):
+    print(f"Число Фибоначчи {i}: {fibonacci(i)}")
