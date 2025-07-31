e34752783bbea76559c38522b7d158d2ebd6e08e
SillyCat96 <alexey.kozhevatov@mail.ru>
2025-08-01 01:07:31 +0300

    change  __doc__ for 16679

--- leetcode tasks programm 75/1679. Max Number of K-Sum Pairs.py
+++ leetcode tasks programm 75/1679. Max Number of K-Sum Pairs.py
@@ -1,3 +1,24 @@
+"""
+You are given an integer array nums and an integer k.
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+Return the maximum number of operations you can perform on the array.
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+Example 2:
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+"""
 from collections import Counter
 
 class Solution(object):
@@ -7,36 +28,27 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # Шаг 1: Подсчитываем частоту каждого числа в nums
-        # collections.Counter - это специализированный словарь для подсчета хешируемых объектов.
         num_counts = Counter(nums)
         operations = 0
         
-        # Шаг 2: Итерируем по уникальным числам и их количествам в Counter
-        # Используем list(num_counts.keys()) чтобы избежать ошибки "RuntimeError: dictionary changed size during iteration"
-        # если мы будем изменять num_counts внутри цикла
         for num in list(num_counts.keys()):
-            complement = k - num # Вычисляем "дополнение", которое необходимо для суммы k
+            complement = k - num 
             
             if num_counts[num] > 0 and num_counts[complement] > 0:
                 
                 if num == complement:
-                    # Случай 1: Число равно своему дополнению (например, k=6, num=3, complement=3)
-                    # Мы можем сформировать пары только из четного количества таких чисел.
-                    # Например, если у нас 3 тройки, мы можем сделать только одну пару (3,3).
                     ops_for_num = num_counts[num] // 2
                     operations += ops_for_num
-                    # Уменьшаем количество использованных чисел в Counter
                     num_counts[num] -= (ops_for_num * 2) 
                 else:
-                    # Случай 2: Число и его дополнение разные (например, k=5, num=1, complement=4)
-                    # Количество пар, которые мы можем сформировать, ограничено наименьшим 
-                    # количеством из этих двух чисел.
                     ops_for_pair = min(num_counts[num], num_counts[complement])
                     operations += ops_for_pair
                     
-                    # Уменьшаем количество использованных чисел в Counter для обоих чисел
                     num_counts[num] -= ops_for_pair
                     num_counts[complement] -= ops_for_pair
                     
         return operations
+
+
+solver = Solution()
+print(solver.maxOperations([1,2,3,4],5))
--- temp.py
+++ temp.py
@@ -1,32 +1,54 @@
 """
-Given an array of integers nums, calculate the pivot index of this array.
-The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the
- sum of all the numbers strictly to the index's right.
-If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. 
-This also applies to the right edge of the array.
+You are given an integer array nums and an integer k.
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+Return the maximum number of operations you can perform on the array.
 
-Return the leftmost pivot index. If no such index exists, return -1.
-Input: nums = [1,7,3,6,5,6]
-Output: 3
-Explanation:
-The pivot index is 3.
-Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
-Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+Example 2:
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
 """
+from collections import Counter
 
-class Solution:
-    def pivotIndex(self, nums):
+class Solution(object):
+    def maxOperations(self, nums, k):
         """
-        Находит самый левый поворотный индекс в массиве.
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
         """
-        total_sum = sum(nums)
-        left_sum = 0
+        num_counts = Counter(nums)
+        operations = 0
         
-        for i, num in enumerate(nums):
-            right_sum = total_sum - left_sum - num
-            if left_sum == right_sum:
-                return i
-            left_sum += num
+        for num in list(num_counts.keys()):
+            complement = k - num 
             
-        return -1
+            if num_counts[num] > 0 and num_counts[complement] > 0:
+                
+                if num == complement:
+                    ops_for_num = num_counts[num] // 2
+                    operations += ops_for_num
+                    num_counts[num] -= (ops_for_num * 2) 
+                else:
+                    ops_for_pair = min(num_counts[num], num_counts[complement])
+                    operations += ops_for_pair
+                    
+                    num_counts[num] -= ops_for_pair
+                    num_counts[complement] -= ops_for_pair
+                    
+        return operations
 
+
+solver = Solution()
+print(solver.maxOperations([1,2,3,4],5))
